3cd8dcc9d3498c5d8352fae2f1e287f5f92f9929
Jacob Nelson <jacobcn716@gmail.com>
2024-05-21 01:51:53 -0700

    finished expose

--- assets/scripts/RecipeCard.js
+++ assets/scripts/RecipeCard.js
@@ -120,7 +120,10 @@ class RecipeCard extends HTMLElement {
 		if (!data) return;
 
 		// A6. TODO - Select the <article> we added to the Shadow DOM in the constructor
-			
+		const shadow = this.shadowRoot;
+		//console.log(this);
+		//console.log(shadow);
+		let article = shadow.children[0];
 		// A7. TODO - Set the contents of the <article> with the <article> template given in
 		//           cardTemplate.html and the data passed in (You should only have one <article>,
 		//           do not nest an <article> inside another <article>). You should use template
@@ -128,6 +131,21 @@ class RecipeCard extends HTMLElement {
 		// 			 Do NOT include the <article> tags within the innerHTML of the element you create.
 		//           Remember to replace all the placeholders in the template with the data passed in.
 		//           i.e. imgSrc, titleLnk, etc
+		article.innerHTML = `<img src=${data.imgSrc}
+		alt=${data.imgAlt}>
+	  <p class="title">
+		<a href=${data.titleLnk}>${data.titleTxt}</a>
+	  </p>
+	  <p class="organization">${data.organization}</p>
+	  <div class="rating">
+		<span>${data.rating}</span>
+		<img src="/assets/images/icons/${data.rating}-star.svg" alt="${data.rating} stars">
+		<span>(${data.numRatings})</span>
+	  </div>
+	  <time>${data.lengthTime}</time>
+	  <p class="ingredients">
+		${data.ingredients}
+	  </p>`
 	}
 }
 
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,12 @@ function getRecipesFromStorage() {
 	// A9. TODO - Complete the functionality as described in this function
 	//           header. It is possible in only a single line, but should
 	//           be no more than a few lines.
+	let recipes = localStorage?.getItem("recipes")
+	if (recipes == undefined) {
+		return [];
+	}
+	return JSON.parse(recipes);
+	
 }
 
 /**
@@ -35,10 +41,16 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
 	// A10. TODO - Get a reference to the <main> element
+	let main = document.querySelector("main");
 	// A11. TODO - Loop through each of the recipes in the passed in array,
 	//            create a <recipe-card> element for each one, and populate
 	//            each <recipe-card> with that recipe data using element.data = ...
 	//            Append each element to <main>
+	for (const x of recipes) {
+		let recipe = document.createElement("recipe-card");
+		recipe.data = x
+		main.append(recipe);
+	}
 }
 
 /**
@@ -51,6 +63,13 @@ function saveRecipesToStorage(recipes) {
 	// B1. TODO - Complete the functionality as described in this function
 	//            header. It is possible in only a single line, but should
 	//            be no more than a few lines.
+	
+	let array = JSON.parse(localStorage.getItem("recipes"));
+	for (const x of recipes) {
+		array.append(x);
+	}
+	localStorage.setItem("recipes", JSON.stringify(array));
+
 }
 
 /**
